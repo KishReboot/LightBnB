ac0b4868d6995e09bd4d50efb427811b257bb0bc
KishReboot <russellsalopek@pm.me>
2022-12-15 19:33:02 -0500

    Editted function addUser to use pool.query, using SQL to enter new values upon sign up.

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -62,11 +62,17 @@ exports.getUserWithId = getUserWithId;
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser =  function(user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
-}
+  return pool
+  .query(`INSERT INTO users(name, email, password) 
+  VALUES ($1, $2, $3) 
+  RETURNING *;`, [user.name, user.password, user.email])
+  .then ((result) => {
+    return result.rows[0];
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+};
 exports.addUser = addUser;
 
 /// Reservations
